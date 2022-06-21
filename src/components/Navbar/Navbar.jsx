e4d87275a06fd153133bdc24e8ca432df279fd43
AntonioTepsich <anto.tepsich@gmail.com>
2022-06-20 23:53:26 -0300

    NavBar terminado

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,19 +12,19 @@ export const Navbar = () => {
         </div>
         <div className=' flex items-center gap-8'>
             <ul className='flex items-center gap-8 '>
-                <li className='text-base text-Color cursor-pointer hover:text-headingColor duration-100'>Inicio</li>
-                <li className='text-base text-Color cursor-pointer hover:text-headingColor duration-100'>Testimonios</li>
-                <li className='text-base text-Color cursor-pointer hover:text-headingColor duration-100'>Sobre Nosotros</li>
-                <li className='text-base text-Color cursor-pointer hover:text-headingColor duration-100'>Buzos</li>
+                <li className='text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Inicio</li>
+                <li className='text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Testimonios</li>
+                <li className='text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Nosotros</li>
+                <li className='text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Buzos</li>
             </ul>
 
             <div className='relative flex items-center justify-center'>
-                <BsFillBasket3Fill className='text-textColor text-2xl cursor-pointer' />
-                <div className=' absolute -top-2 -right-8 w-5 h-5 rounded-full bg-cartNumBg flex items-center  justify-center'>
+                <BsFillBasket3Fill className='text-textColor text-2xl  cursor-pointer' />
+                <div className=' absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center  justify-center'>
                     <p className='text-xs text-white font-semibold'>2</p>
                 </div>
             </div>
-            <img src={avatar} className='w-9 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl' alt="usuario" />
+            <img src={avatar} className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full' alt="usuario" />
         </div>
       </div>
       <div className='flex md:hidden w-full h-full '>
